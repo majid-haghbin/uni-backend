ed4a3600b0b4cea8e5fcbeaba1acf17e43a90f1c
Majid Haghbin <majid.haghbin.mh@gmail.com>
2023-12-26 17:24:52 +0330

    feat: change port the of server and make server not crashing on errors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+
+const PORT = 3030
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false
+  })
+
+  await app.listen(PORT)
 }
-bootstrap();
+bootstrap()
