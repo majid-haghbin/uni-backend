3402d04f5ddcf3073dc3c167030ec96ed237d921
Majid Haghbin <majid.haghbin.mh@gmail.com>
2023-12-27 19:24:27 +0330

    feat: use of ConfigModule and config it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      /** نیازی به ایمپورت کردن در مابقی ماژول‌ها نباشد */
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
