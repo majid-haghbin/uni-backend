bfe4cf7a3e7827a8359a3d3c948b7bd792832a94
Majid Haghbin <majid.haghbin.mh@gmail.com>
2023-12-27 19:22:37 +0330

    feat: change config of ValidationPipe and add tsdoc

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false
   })
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    /** 
+     * مقدار زیر تعیین می‌کند که خطاهای 400 با جزئیات نمایش داده شوند یا خیر
+     * @todo باید بر اساس اینکه در پروداکشن هستیم یا دولوپمنت پیاده سازی شود
+     */
+    disableErrorMessages: false,
+  }));
 
   await app.listen(PORT)
 }
