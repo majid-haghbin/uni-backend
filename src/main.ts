0cb957f82d0a7b24e6934800b55683e9e6cbbf05
Majid Haghbin <majid.haghbin.mh@gmail.com>
2023-12-27 14:28:05 +0330

    feat(main.ts): use of ValidationPipe global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { ValidationPipe } from '@nestjs/common'
 
 const PORT = 3030
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false
   })
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT)
 }
