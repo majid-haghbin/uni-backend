f9ca785b6070e2d35ee0bb9e29b8b286fba6fab4
Majid Haghbin <majid.haghbin.mh@gmail.com>
2024-01-19 18:05:16 +0330

    feat(auth.service.ts): add file and implement signIn method

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import { jwtConstants } from './constants'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findUser({
+      where: { mobile: username }
+    })
+
+    if (user.password !== password) {
+      throw new ForbiddenException('نام کاربری یا رمز عبور اشتباه است')
+    }
+
+    const payload = { sub: user.ID, username: user.mobile }
+
+    return {
+      body: {
+        access_token: await this.jwtService.signAsync(payload, {
+          secret: jwtConstants.secret
+        })
+      }
+    }
+  }
+}
