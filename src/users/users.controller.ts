fdf79133b586e6d987825e480f3857735c2e3e02
Majid Haghbin <majid.haghbin.mh@gmail.com>
2024-01-19 18:48:19 +0330

    feat: create admin guard and use of it

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post } from '@nestjs/common'
+import { Controller, Post, UseGuards } from '@nestjs/common'
 import { UsersService } from './users.service'
+import { AdminGuard } from 'src/auth/admin.guard'
 
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AdminGuard)
   @Post('list')
   async usersList() {
     const users = await this.usersService.usersList()
