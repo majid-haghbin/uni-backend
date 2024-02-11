0245afdb0a77ed8a78437e9402b51f270088d718
Majid Haghbin <majid.haghbin.mh@gmail.com>
2024-02-11 17:11:51 +0330

    feat: add get /user/profile api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { AuthGuard } from 'src/auth/auth.guard'
+import { RequestWithUser } from 'type'
+import { AppService } from 'src/app.service'
 
 @Controller('user')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly appService: AppService
+  ) {}
 
   @UseGuards(AuthGuard(['superAdmin', 'admin']))
   @Post('list')
   async usersList() {
     const users = await this.usersService.usersList()
     return users
+  }
+
+  @UseGuards(AuthGuard(['superAdmin', 'admin', 'professor', 'student']))
+  @Get('profile')
+  async getProfile(@Req() request: RequestWithUser) {
+    const profile = request.user
+
+    return this.appService.myResponse({ profile })
   }
 }
